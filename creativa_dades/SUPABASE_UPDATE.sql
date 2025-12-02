134f87366f6216e263c68b2b3556aa3b247c51b2
raularpal <raularpalsanchez@gmail.com>
2025-12-02 16:42:47 +0100

    Delete creativa_dades directory

--- creativa_dades/SUPABASE_UPDATE.sql
+++
@@ -1,5 +0,0 @@
--- Ejecuta este código en el SQL Editor de tu proyecto en Supabase
--- para añadir las columnas necesarias para el descuento y las observaciones.
-
-ALTER TABLE invoices ADD COLUMN discount numeric DEFAULT 0;
-ALTER TABLE invoices ADD COLUMN observations text;
